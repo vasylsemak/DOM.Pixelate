920e2b1e3b6c9afcbd1cd81fe54c21205aa89298
Vasyl Semak <vasylsmk1401@gmail.com>
2020-08-31 19:38:40 -0400

    add eventlisteners to makeRow, colorize

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Your code here
+const table = document.getElementsByTagName('table')[0];
+const addRowBtn = document.getElementById('add-row');
+const selector = document.getElementsByTagName('select')[0];
+
+// console.log('HHH', selector);
+
+const makeRow = dataQ => {
+  const tRow = document.createElement('tr');
+  for (let i = 0; i < dataQ; i++) {
+    const tData = document.createElement('td');
+    tRow.appendChild(tData);
+  }
+  table.appendChild(tRow);
+};
+
+const colorize = (evt, color) => evt.target.classList.toggle(color);
+
+addRowBtn.addEventListener('click', () => makeRow(10));
+table.addEventListener('click', event => colorize(event, 'red'));
+selector.addEventListener('change', event => {
+  console.log(event.target);
+});
+
+// makeRow(20);
+// makeRow(20);
