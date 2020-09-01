c118b6801cda147934cb8c5ef9e39d86e78eea26
Vasyl Semak <vasylsmk1401@gmail.com>
2020-08-31 20:36:59 -0400

    fix bug: prevent border colorize

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const table = document.getElementsByTagName('table')[0];
 const addRowBtn = document.getElementById('add-row');
 const selector = document.getElementsByTagName('select')[0];
-
-// console.log('HHH', selector);
+let pickColor = 'red';
 
 const makeRow = dataQ => {
   const tRow = document.createElement('tr');
+
   for (let i = 0; i < dataQ; i++) {
     const tData = document.createElement('td');
     tRow.appendChild(tData);
@@ -13,13 +13,11 @@ const makeRow = dataQ => {
   table.appendChild(tRow);
 };
 
-const colorize = (evt, color) => evt.target.classList.toggle(color);
+const colorize = (evt, color) => {
+  // event.target.matches('td') evt.target.tagName === 'TD'
+  if (evt.target.tagName === 'TD') evt.target.classList.toggle(color);
+};
 
 addRowBtn.addEventListener('click', () => makeRow(10));
-table.addEventListener('click', event => colorize(event, 'red'));
-selector.addEventListener('change', event => {
-  console.log(event.target);
-});
-
-// makeRow(20);
-// makeRow(20);
+selector.addEventListener('change', event => (pickColor = event.target.value));
+table.addEventListener('click', event => colorize(event, pickColor));
